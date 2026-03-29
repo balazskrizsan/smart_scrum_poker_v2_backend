a9a96cc87b1ed2b631264964a1219bd738fd2580
Balazs Krizsan <krizsan.balazs@gmail.com>
2026-03-30 00:56:58 +0200

    Tests temp deleted

--- src/test/java/org/kbalazs/smart_scrum_poker_backend_native/helpers/TestException.java
+++
@@ -1,8 +0,0 @@
-package org.kbalazs.smart_scrum_poker_backend_native.helpers;
-
-import lombok.experimental.StandardException;
-
-@StandardException
-public class TestException extends Exception
-{
-}
--- src/test/java/org/kbalazs/smart_scrum_poker_backend_native/helpers/exceptions/ServiceFactoryException.java
+++
@@ -1,8 +0,0 @@
-package org.kbalazs.smart_scrum_poker_backend_native.helpers.exceptions;
-
-import lombok.experimental.StandardException;
-
-@StandardException
-public class ServiceFactoryException extends Exception
-{
-}
