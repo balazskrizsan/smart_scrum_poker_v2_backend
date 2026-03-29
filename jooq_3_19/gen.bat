0623302e8af09e772b0612c491236fa1731b9eff
Balazs Krizsan <krizsan.balazs@gmail.com>
2026-03-30 00:57:28 +0200

    JOOQ version upgrade

---
+++ jooq_3_19/gen.bat
@@ -0,0 +1,3 @@
+"W:\Java_runner\graalvm-jdk-21.0.3+7.1\bin\java.exe" -cp "jooq-3.19.21.jar;jooq-meta-3.19.21.jar;jooq-codegen-3.19.21.jar;reactive-streams-1.0.4.jar;C:\Users\kb\.m2\repository\org\postgresql\postgresql\42.7.3\postgresql-42.7.3.jar;jakarta.xml.bind-api-3.0.1.jar;jaxb-runtime-3.0.1.jar;r2dbc-spi-1.0.0.RELEASE.jar;" org.jooq.codegen.GenerationTool ./gen-config.xml
+
+
